2016c88c9da237edb48cd3942921bbcf8e195754
bel1c10ud <86952779+bel1c10ud@users.noreply.github.com>
2023-12-26 12:44:20 +0900

    [5] localStorage 유틸 함수 작성 (#8)
    * Feat: localStorage 공용 유틸 함수 작성
    
    * Refactor: 오타 수정 (defualtValue -> defaultValue)

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,40 @@
+const storage = window.localStorage;
+
+export const getItem = <T>(key: string, defaultValue: T) => {
+  try {
+    const storedValue = storage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : defaultValue;
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(e.message);
+    } else {
+      console.error('localStorage: 알 수 없는 에러가 발생했습니다. (getItem)');
+    }
+  }
+};
+
+export const setItem = <T>(key: string, value: T) => {
+  try {
+    storage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(e.message);
+    } else {
+      console.error('localStorage: 알 수 없는 에러가 발생했습니다. (setItem)');
+    }
+  }
+};
+
+export const removeItem = (key: string) => {
+  try {
+    storage.removeItem(key);
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(e.message);
+    } else {
+      console.error(
+        'localStorage: 알 수 없는 에러가 발생했습니다. (removeItem)'
+      );
+    }
+  }
+};
