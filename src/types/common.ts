956ad7f567f84a90210693bde3d0777f1c004b79
nnahyunn <nahyun0104@cau.ac.kr>
2023-12-26 10:48:16 +0900

    Feat: 공통 API 타입 정의 파일 추가

---
+++ src/types/common.ts
@@ -0,0 +1,92 @@
+export interface UserType {
+  coverImage: string; // 커버 이미지
+  image: string; // 프로필 이미지
+  role: string;
+  emailVerified: boolean; // 사용되지 않음
+  banned: boolean; // 사용되지 않음
+  isOnline: boolean;
+  posts: PostType[];
+  likes: LikeType[];
+  comments: string[];
+  followers: [];
+  following: [
+    {
+      _id: string;
+      user: string;
+      follower: string;
+      createdAt: string;
+      updatedAt: string;
+      __v: 0;
+    },
+  ];
+  notifications: Notification[];
+  messages: MessageType[];
+  _id: string;
+  fullName: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface ChannelType {
+  authRequired: boolean; // 사용되지 않음
+  posts: string[];
+  _id: string;
+  name: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface PostType {
+  likes: LikeType[];
+  comments: Comment[];
+  _id: string;
+  image?: string;
+  imagePublicId?: string;
+  title: string;
+  channel: ChannelType;
+  author: UserType;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface LikeType {
+  _id: string;
+  user: string; // 사용자 id
+  post: string; // 포스트 id
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CommentType {
+  _id: string;
+  comment: string;
+  author: UserType;
+  post: string; // 포스트 id
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface NotificationType {
+  seen: boolean;
+  _id: string;
+  author: UserType;
+  user: UserType | string;
+  post?: string; // 포스트 id
+  follow?: string; // 사용자 id
+  comment?: Comment;
+  message?: string; // 메시지 id
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface MessageType {
+  _id: string;
+  message: string;
+  sender: UserType;
+  receiver: UserType;
+  seen: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
