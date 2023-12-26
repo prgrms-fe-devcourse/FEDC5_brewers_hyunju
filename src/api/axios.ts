692386b3e9290316ff0e6bf32c253eef59d21cef
yeonkyoung <keep.ykk@gmail.com>
2023-12-26 10:44:30 +0900

    Feat: axios instance에서 공통으로 사용할 request 함수 생성

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,18 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
+
+export const request = async (config: AxiosRequestConfig) => {
+  try {
+    const response = await axiosInstance.request({
+      ...config,
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.status >= 200 && response.status <= 299) return response.data;
+  } catch (error: unknown) {
+    console.error(error);
+  }
+};
