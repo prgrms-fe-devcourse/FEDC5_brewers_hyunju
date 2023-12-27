0226ee054f443899df4a9c64fc5f53273c4cd962
yeonkyoung <keep.ykk@gmail.com>
2023-12-27 09:39:40 +0900

    [3] axios 공통으로 사용되는 request 함수 추가 (#9)
    * Feat: axios instance에서 공통으로 사용할 request 함수 생성
    
    * Feat: error message 상수 파일 생성 및 API_REQUEST_ERROR 메시지 추가
    
    * Feat: axios request 200번대가 아닌 경우, throw error
    
    * Refactor: config로 headers를 변경할 수 있도록 config위치 변경
    
    * Refactor: error를 e로 수정, e의 타입 검사 추가
    
    * Refactor: early return 형식으로 수정
    
    * Refactor: 파일명 error에서 message로 수정, 도메인별로 객체 정의
    
    * Refactor: 수정된 API request error 객체 반영

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,27 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
+import { API } from '~/constants/message';
 
 export const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
+
+export const request = async (config: AxiosRequestConfig) => {
+  try {
+    const response = await axiosInstance.request({
+      headers: { 'Content-Type': 'application/json' },
+      ...config,
+    });
+
+    if (response.status < 200 || response.status > 299) {
+      throw new Error(API.REQUEST_ERROR);
+    }
+
+    return response.data;
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(e.message);
+    } else {
+      console.error(e);
+    }
+  }
+};
