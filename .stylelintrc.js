daf9246fa110ec19691679b26dd64cebc50965d8
bel1c10ud <mail@bellclou.de>
2023-12-28 23:12:24 +0900

    Chore: 전역 색상 사용을 강제하는 stylelint 규칙 추가

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+export default {
+  extends: ['stylelint-config-standard', 'stylelint-styled-components-order'],
+  rules: {
+    'color-no-hex': true,
+    'color-named': 'never',
+    'declaration-property-value-disallowed-list': {
+      '/.+/': '/(rgba?|hsla?)(.*)/',
+    },
+  },
+};
