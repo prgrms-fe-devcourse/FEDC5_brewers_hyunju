9972a61891aa919495adb9b9c2c5ba67fab2de55
nnahyunn <nahyun0104@cau.ac.kr>
2023-12-29 13:21:51 +0900

    Refactor: 전체 검색 기능을 위한 useSearchAll 훅 파일 분리 및 로직 개선

---
+++ src/hooks/api/search/useSearchAll.ts
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { request } from '~/api/axios';
+import { getItem } from '~/utils/localStorage';
+import { handleError } from '~/utils/handleError';
+import { SearchTotalResponseType } from '~/types/api/search';
+
+export const useSearchAll = (query: string) => {
+  const [status, setStatus] = useState<
+    'stale' | 'loading' | 'error' | 'success'
+  >('stale');
+  const [results, setResults] = useState<SearchTotalResponseType | null>(null);
+  const searchAll = async () => {
+    if (!query) {
+      return;
+    }
+    const SEARCH_ALL_URL = `/search/all/${encodeURIComponent(query)}`;
+    const userToken = getItem<string | null>('userToken', null);
+    setStatus('loading');
+    try {
+      const response = await request({
+        method: 'GET',
+        url: SEARCH_ALL_URL,
+        headers: {
+          ...(userToken ? { Authorization: `Bearer ${userToken}` } : {}),
+        },
+      });
+      setResults(response.data);
+      setStatus('success');
+    } catch (e: unknown) {
+      handleError(e, 'SearchAll');
+      setStatus('error');
+    }
+  };
+
+  return { status, results, searchAll };
+};
+
+export default useSearchAll;
