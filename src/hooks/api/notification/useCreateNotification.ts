83098a035ea38a654cd8b37dc51c7e2ac0229329
nnahyunn <nahyun0104@cau.ac.kr>
2023-12-29 13:28:24 +0900

    Refactor: 알림 생성 기능을 위한 useCreateNotification 훅 파일 분리 및 로직 개선

---
+++ src/hooks/api/notification/useCreateNotification.ts
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { request } from '~/api/axios';
+import { getItem } from '~/utils/localStorage';
+import { handleError } from '~/utils/handleError';
+import {
+  CreateNotificationRequestType,
+  CreateNotificationResponseType,
+} from '~/types/api/notification';
+
+export const useCreateNotification = () => {
+  const [status, setStatus] = useState<
+    'stale' | 'loading' | 'error' | 'success'
+  >('stale');
+  const [notification, setNotification] =
+    useState<CreateNotificationResponseType | null>(null);
+  const CREATE_NOTIFICATION_URL = '/notifications/create';
+
+  const createNotification = async ({
+    notificationType,
+    notificationTypeId,
+    userId,
+    postId,
+  }: CreateNotificationRequestType) => {
+    setStatus('loading');
+    const userToken = getItem<string | null>('userToken', null);
+    try {
+      const response = await request({
+        method: 'POST',
+        url: CREATE_NOTIFICATION_URL,
+        headers: {
+          ...(userToken ? { Authorization: `Bearer ${userToken}` } : {}),
+        },
+        data: {
+          notificationType,
+          notificationTypeId,
+          userId,
+          postId,
+        },
+      });
+      setNotification(response.data);
+      setStatus('success');
+    } catch (e: unknown) {
+      handleError(e, 'CreateNotification');
+      setStatus('error');
+    }
+  };
+
+  return { status, notification, createNotification };
+};
+
+export default useCreateNotification;
