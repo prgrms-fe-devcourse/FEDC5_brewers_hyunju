32f7779af9c3e3ee51e6524af7af549c8f53a326
bel1c10ud <86952779+bel1c10ud@users.noreply.github.com>
2024-01-01 11:29:29 +0900

    [18] storybook 테스팅을 위한 다크모드 구현 방식 변경 (#19)
    * Style: 다크모드 구현방식 변경
    
    * Chore: storybook-dark-mode addon 설치
    
    * Fix: 필요없는 공백 제거

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,19 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+import './../src/index.css';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    backgrounds: { disable: true },
+    darkMode: {
+      classTarget: 'html',
+      stylePreview: true,
     },
   },
 };
