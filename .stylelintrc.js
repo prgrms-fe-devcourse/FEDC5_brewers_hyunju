c5b79f4486b24951e922953e5961cc27822510fb
bel1c10ud <mail@bellclou.de>
2024-01-01 17:34:16 +0900

    Fix: 잘못된 규칙 값 수정 (false -> null)

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -6,6 +6,6 @@ export default {
     'declaration-property-value-disallowed-list': {
       '/.+/': '/(rgba?|hsla?)(.*)/',
     },
-    'declaration-block-no-redundant-longhand-properties': false,
+    'declaration-block-no-redundant-longhand-properties': null,
   },
 };
