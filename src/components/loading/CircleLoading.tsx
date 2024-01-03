3ed1026837a9dd2e48f8510f832b0db8ef604f1f
yeonkyoung <keep.ykk@gmail.com>
2024-01-03 10:45:07 +0900

    Feat: Loading 컴포넌트 구현

---
+++ src/components/loading/CircleLoading.tsx
@@ -0,0 +1,66 @@
+import styled, { keyframes } from 'styled-components';
+import ColorType from '~/types/design/color';
+
+interface CircleLoadingPropsType {
+  size?: number;
+  time?: number;
+  primaryColor?: ColorType;
+  secondaryColor?: ColorType;
+}
+
+const CircleLoading = ({
+  size = 7.5,
+  time = 2,
+  primaryColor = '--primaryColor' as ColorType,
+  secondaryColor = '--adaptive300' as ColorType,
+}: CircleLoadingPropsType) => {
+  const newSize = size < 2 ? 2 : size;
+  const convertedSize = newSize * 16;
+  const circleRadius = convertedSize / 4;
+  const primaryColorBarRatio = (circleRadius * 2 * Math.PI) / 5;
+  const secondaryColorBarRatio = ((circleRadius * 2 * Math.PI) / 5) * 4;
+
+  const rotateBar = keyframes`
+    100% {
+        transform: rotate(360deg);
+}
+  `;
+
+  const Container = styled.div`
+    width: ${newSize}rem;
+    height: ${newSize}rem;
+
+    animation: ${rotateBar} ${time}s linear infinite;
+  `;
+
+  return (
+    <Container>
+      <svg
+        width={convertedSize}
+        height={convertedSize}
+        viewBox={`0 0 ${convertedSize} ${convertedSize}`}
+      >
+        <circle
+          cx={circleRadius * 2}
+          cy={circleRadius * 2}
+          r={circleRadius}
+          stroke-width='12'
+          fill='none'
+          style={{ stroke: `var(${secondaryColor})` }}
+        />
+        <circle
+          cx={circleRadius * 2}
+          cy={circleRadius * 2}
+          r={circleRadius}
+          stroke-width='12'
+          fill='none'
+          style={{ stroke: `var(${primaryColor})` }}
+          stroke-dasharray={`${primaryColorBarRatio} ${secondaryColorBarRatio}`}
+          stroke-linecap='round'
+        />
+      </svg>
+    </Container>
+  );
+};
+
+export default CircleLoading;
---
+++ src/stories/components/CircleLoading.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import CircleLoading from '~/components/loading/CircleLoading';
+
+const meta: Meta<typeof CircleLoading> = {
+  title: 'Components/Common/CircleLoading',
+  component: CircleLoading,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof CircleLoading>;
+
+export const Standard: Story = {
+  args: {
+    size: 7.5,
+    time: 2,
+    primaryColor: '--primaryColor',
+    secondaryColor: '--adaptive300',
+  },
+};
