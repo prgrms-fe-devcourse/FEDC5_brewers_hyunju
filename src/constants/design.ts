aa57b24ddf288f7deb9dd8302fe52d30daa27390
bel1c10ud <mail@bellclou.de>
2024-01-03 16:23:39 +0900

    Style: CSS 전역 색상 추가

--- src/constants/design.ts
+++ src/constants/design.ts
@@ -9,6 +9,9 @@ import {
 export const COLOR: ColorType[] = [
   '--primaryColor',
   '--secondaryColor',
+  '--white',
+  '--black',
+  '--ransparent',
   '--adaptive50',
   '--adaptive100',
   '--adaptive200',
