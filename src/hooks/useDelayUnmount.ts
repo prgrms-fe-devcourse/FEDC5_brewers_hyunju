a154a9f2092b8aad3c0560609c902a2fb2802829
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-01-04 19:05:57 +0900

    Feat: unmount 애니메이션 custom hook 구현

---
+++ src/hooks/useDelayUnmount.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+
+const useDelayUnmount = (isMounted: boolean, delay: number) => {
+  const [shouldRender, setShouldRender] = useState(false);
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+    if (isMounted && !shouldRender) {
+      setShouldRender(true);
+    } else if (!isMounted && shouldRender) {
+      timeoutId = setTimeout(() => setShouldRender(false), delay);
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [delay, isMounted, shouldRender]);
+  return shouldRender;
+};
+
+export default useDelayUnmount;
