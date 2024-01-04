063a45fe5916e2feb130f5a5adc5c6a26961f563
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-01-04 17:13:01 +0900

    Docs: Tabs 스토리 추가

--- src/stories/Tabs.stories.tsx
+++ src/stories/Tabs.stories.tsx
@@ -1,3 +1,4 @@
+import { IconUser } from '@tabler/icons-react';
 import styled from 'styled-components';
 import Tabs, { TabsPropsType } from '~/components/common/Tabs';
 import { FONT_SIZE_UNIT, FONT_WEIGHT_UNIT } from '~/constants/design';
@@ -11,9 +12,7 @@ export default {
     fontSize: { control: 'select', options: FONT_SIZE_UNIT },
     fontWeight: { control: 'select', options: FONT_WEIGHT_UNIT },
   },
-  args: {
-    isFull: false,
-  },
+  args: {},
 };
 export const Default = (args: TabsPropsType) => {
   return (
@@ -38,6 +37,58 @@ export const Default = (args: TabsPropsType) => {
   );
 };
 
+export const MinimumTab = (args: TabsPropsType) => {
+  return (
+    <Box>
+      <Tabs {...args}>
+        <Tabs.Header>
+          <Tabs.Item
+            text='1'
+            id={0}
+          />
+          <Tabs.Item
+            text='TAB 2'
+            id={1}
+          />
+          <Tabs.Item
+            text='TAB 3'
+            id={2}
+          />
+        </Tabs.Header>
+      </Tabs>
+    </Box>
+  );
+};
+
+export const IconTab = (args: TabsPropsType) => {
+  return (
+    <Box>
+      <Tabs {...args}>
+        <Tabs.Header>
+          <Tabs.Item
+            id={0}
+            icon={
+              <IconUser
+                color='var(--primaryColor)'
+                stroke={3}
+              />
+            }
+          />
+          <Tabs.Item
+            text='TAB 2'
+            id={1}
+          />
+          <Tabs.Item
+            text='TAB 3'
+            id={2}
+          />
+        </Tabs.Header>
+      </Tabs>
+    </Box>
+  );
+};
+
 const Box = styled.div`
   width: 500px;
+  border: solid 1px var(--secondaryColor);
 `;
