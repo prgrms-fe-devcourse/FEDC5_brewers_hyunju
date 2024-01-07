80a6a4dc9efeb528cd82d83a482dbeb9fb6ab7ac
Jihee Shin <jiheenim27@gmail.com>
2024-01-07 16:36:17 +0900

    [36] Radio Button 컴포넌트 구현 (#48)
    * Feat: Radio 컴포넌트 구현
    
    * Feat: RadioGroup 컴포넌트 구현
    
    * Refactor: label 출력 위치 수정, props에 onClick 추가
    
    * Docs: RadioGroup 스토리북 argTypes 변경 (주석 제거)

---
+++ src/components/RadioGroup/Radio.tsx
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import Flex from '~/components/common/Flex';
+import Text from '~/components/common/Text';
+
+export interface RadioPropsType {
+  children?: string;
+  value: string;
+  name: string;
+  defaultChecked?: boolean;
+  disabled?: boolean;
+  checked?: boolean;
+  onChange: () => void;
+}
+
+const RadioButtonInput = styled.input`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+
+  width: 2.5rem;
+  height: 2.5rem;
+  outline: none;
+  border: 3px solid var(--adaptive500);
+  border-radius: 50%;
+
+  color: var(--adaptive500);
+
+  appearance: none;
+
+  cursor: pointer;
+
+  &:checked {
+    border: 4px solid var(--primaryColor);
+    color: var(--primaryColor);
+
+    &::after {
+      display: block;
+      position: absolute;
+
+      width: 1.24rem;
+      height: 1.24rem;
+      border-radius: 50%;
+
+      background-color: var(--primaryColor);
+
+      content: '';
+    }
+  }
+`;
+
+const Radio = ({
+  children,
+  value,
+  name,
+  defaultChecked,
+  disabled,
+  checked,
+  onChange,
+}: RadioPropsType) => {
+  return (
+    <Flex
+      as='fieldset'
+      direction='column'
+      alignItems='center'
+      gap={1}
+    >
+      <RadioButtonInput
+        type='radio'
+        value={value}
+        name={name}
+        defaultChecked={defaultChecked}
+        disabled={disabled}
+        checked={checked}
+        onChange={onChange}
+      ></RadioButtonInput>
+      <Text as='label'>{children}</Text>
+    </Flex>
+  );
+};
+
+export default Radio;
---
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Container from '~/components/common/Container';
+import Flex from '~/components/common/Flex';
+import Radio from './Radio';
+
+export interface RadioGroupPropsType {
+  options: { label: string; value: string }[];
+  defaultValue?: string;
+  disabled?: boolean;
+  onChange?: (value: string) => void;
+}
+
+const RadioGroup: React.FC<RadioGroupPropsType> = ({
+  options,
+  defaultValue,
+  disabled,
+  onChange,
+}) => {
+  const [selectedValue, setSelectedValue] = useState<string | undefined>(
+    defaultValue
+  );
+
+  const handleRadioChange = (value: string) => {
+    setSelectedValue(value);
+    if (onChange) {
+      onChange(value);
+    }
+  };
+
+  return (
+    <Container maxWidth='sm'>
+      <Flex
+        justifyContent='space-between'
+        alignItems='flex-start'
+        style={{ padding: '0  0.25rem' }}
+      >
+        {options.map((option) => (
+          <Radio
+            key={option.value}
+            value={option.value}
+            name='radioGroup'
+            checked={selectedValue === option.value}
+            disabled={disabled}
+            onChange={() => handleRadioChange(option.value)}
+          >
+            {option.label}
+          </Radio>
+        ))}
+      </Flex>
+    </Container>
+  );
+};
+
+export default RadioGroup;
