a3f2a6eeb534e32f1bdb2cbbbbc9c20b75b74718
Yongjae Choi <yongjae.choi20@gmail.com>
2024-01-10 10:25:34 +0900

    [99] 스타일 린트 에러 발생 파일 추적 및 수정 (#100)
    * Fix: Linting 누락 버그 fix
    
    * Refactor: styled component emotion 형식으로 전환
    
    * Refactor: import 개행 제거

--- src/components/common/Skeleton.tsx
+++ src/components/common/Skeleton.tsx
@@ -24,70 +24,82 @@ const wave = keyframes`
   }
 `;
 
-const Skeleton = styled.div<SkeletonPropsType>`
+const Skeleton = styled.div(
+  ({ width = 10, height = 1, ...props }: SkeletonPropsType) => `
   ${CommonStyle}
   
-  display: ${(props) => (props.inline ? 'inline-block' : 'block')};
+  display: ${props.inline ? 'inline-block' : 'block'};
 
-  width: ${({ width = 10, height = 1, circle }) =>
-    circle
+  width: ${
+    props.circle
       ? `max(${handleSize(width, 'base')}, ${handleSize(height, 'base')})`
-      : handleSize(width, 'base')};
-  height: ${({ width = 10, height = 1, circle }) =>
-    circle
+      : handleSize(width, 'base')
+  };
+  height: ${
+    props.circle
       ? `max(${handleSize(width, 'base')}, ${handleSize(height, 'base')})`
-      : handleSize(height, 'base')};
-  border-radius: ${({ circle }) => (circle ? '50%' : '0.125rem')};
+      : handleSize(height, 'base')
+  };
+  border-radius: ${props.circle ? '50%' : '0.125rem'};
 
   background-color: var(--adaptive300);
 
   animation: ${wave} 2s infinite linear paused alternate;
 
-  animation-play-state: ${(props) => (props.animation ? 'running' : 'pasued')};
+  animation-play-state: ${props.animation ? 'running' : 'pasued'};
 
   @media screen and (width <= ${CONTAINER_SIZE['sm']}) {
-    width: ${({ width = 10, height = 1, circle }) =>
-      circle
+    width: ${
+      props.circle
         ? `max(${handleSize(width, 'sm')}, ${handleSize(height, 'sm')})`
-        : handleSize(width, 'sm')};
-    height: ${({ width = 10, height = 1, circle }) =>
-      circle
+        : handleSize(width, 'sm')
+    };
+    height: ${
+      props.circle
         ? `max(${handleSize(width, 'sm')}, ${handleSize(height, 'sm')})`
-        : handleSize(height, 'sm')};
+        : handleSize(height, 'sm')
+    };
   }
 
   @media screen and (width > ${CONTAINER_SIZE['sm']}) {
-    width: ${({ width = 10, height = 1, circle }) =>
-      circle
+    width: ${
+      props.circle
         ? `max(${handleSize(width, 'md')}, ${handleSize(height, 'md')})`
-        : handleSize(width, 'md')};
-    height: ${({ width = 10, height = 1, circle }) =>
-      circle
+        : handleSize(width, 'md')
+    };
+    height: ${
+      props.circle
         ? `max(${handleSize(width, 'md')}, ${handleSize(height, 'md')})`
-        : handleSize(height, 'md')};
+        : handleSize(height, 'md')
+    };
   }
 
   @media screen and (width > ${CONTAINER_SIZE['md']}) {
-    width: ${({ width = 10, height = 1, circle }) =>
-      circle
+    width: ${
+      props.circle
         ? `max(${handleSize(width, 'lg')}, ${handleSize(height, 'lg')})`
-        : handleSize(width, 'lg')};
-    height: ${({ width = 10, height = 1, circle }) =>
-      circle
+        : handleSize(width, 'lg')
+    };
+    height: ${
+      props.circle
         ? `max(${handleSize(width, 'lg')}, ${handleSize(height, 'lg')})`
-        : handleSize(height, 'lg')};
+        : handleSize(height, 'lg')
+    };
   }
 
   @media screen and (width > ${CONTAINER_SIZE['lg']}) {
-    width: ${({ width = 10, height = 1, circle }) =>
-      circle
+    width: ${
+      props.circle
         ? `max(${handleSize(width, 'xl')}, ${handleSize(height, 'xl')})`
-        : handleSize(width, 'xl')};
-    height: ${({ width = 10, height = 1, circle }) =>
-      circle
+        : handleSize(width, 'xl')
+    };
+    height: ${
+      props.circle
         ? `max(${handleSize(width, 'xl')}, ${handleSize(height, 'xl')})`
-        : handleSize(height, 'xl')};
+        : handleSize(height, 'xl')
+    };
   }
-`;
+`
+);
 
 export default Skeleton;
