535ad34391e218a3f1f0d8af91248e94672fb5b6
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-01-11 12:28:30 +0900

    Feat: Skeleton 구현

---
+++ src/components/UserListSkeleton.tsx
@@ -0,0 +1,57 @@
+import styled from '@emotion/styled';
+import Skeleton from './common/Skeleton';
+import ListItem from './search/ListItem';
+import Flex from './common/Flex';
+
+const UserListItemSkeleton = () => {
+  return (
+    <ListItem
+      gap={1}
+      justifyContent='space-between'
+    >
+      <Flex
+        gap={1}
+        alignItems='center'
+      >
+        <Skeleton
+          width={2.5}
+          circle={true}
+        />
+        <Skeleton
+          animation
+          height={1}
+          width={15}
+        />
+      </Flex>
+
+      <RoundSkeleton
+        animation
+        height={2}
+        width={5.125}
+      />
+    </ListItem>
+  );
+};
+const UserListSkeleton = () => {
+  return (
+    <StyledUl>
+      {Array.from(Array(5)).map(() => {
+        return <UserListItemSkeleton />;
+      })}
+    </StyledUl>
+  );
+};
+
+export default UserListSkeleton;
+
+const StyledUl = styled.ul`
+  flex-direction: column;
+  align-items: center;
+
+  padding: 1rem;
+`;
+
+const RoundSkeleton = styled(Skeleton)`
+  justify-self: flex-end;
+  border-radius: 1.25rem;
+`;
