1e695cbb277d662e93a605a4ac98f143cb05d925
Jihee Shin <jiheenim27@gmail.com>
2024-01-12 00:34:53 +0900

    Feat: Feed 입력창 구현

--- src/components/feed/FeedListInput.tsx
+++ src/components/feed/FeedListInput.tsx
@@ -1,104 +1,112 @@
-import { useState } from 'react';
+import { ChangeEvent } from 'react';
 import styled from '@emotion/styled';
-import Input from '~/components/input/Input';
 import Avatar from '~/components/common/Avatar';
 import Button from '~/components/common/Button';
 import Flex from '~/components/common/Flex';
-import { FeedItemContainer } from './FeedListItem';
+import FeedContentEditable from '~/components/feed/FeedContentEditable';
+import useFeedContent from '~/hooks/useFeedContent';
+import Container from '~/components/common/Container';
 
 export interface FeedListInputPropsType {
   userId: string;
   profileImage: string;
 }
 
-const FeedInput = styled(Input)`
-  padding-top: 0;
-  margin-top: 0;
+const FeedListInputContainer = styled(Container)`
+  flex-shrink: 0;
+
+  margin-bottom: 3.25rem;
+  padding: 34px 40px;
+  border: 1px solid var(--adaptive200);
+  border-radius: 0.75rem;
+  box-shadow: 0 4px 4px 0 var(--adaptiveOpacity100);
+
+  background-color: var(-adaptive50);
+
+  box-sizing: border-box;
 `;
 
 const FeedListInput = ({ userId, profileImage }: FeedListInputPropsType) => {
-  const [text, setText] = useState('');
+  const { content, removeContent, handleInput } = useFeedContent();
 
-  const onChange = (inputText: string) => {
-    setText(inputText);
-  };
+  const handleButtonClick = () => {
+    if (content) {
+      alert(content);
+      removeContent();
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (text) {
-      alert(text);
-      // post 처리
-      setText('');
+      // 데이터 통신
     }
   };
 
   return (
-    <FeedItemContainer maxWidth='md'>
-      <form onSubmit={onSubmit}>
-        <Flex
-          justifyContent='space-between'
-          alignItems='flex-start'
-        >
-          <div>
-            <Avatar
-              src={profileImage}
-              size='sm'
-              alt='user image'
-              handleClick={() => {
-                alert(userId);
-              }}
-            ></Avatar>
-          </div>
-          <Flex
-            direction='column'
-            ml={1}
-            style={{
-              width: '44rem',
+    <FeedListInputContainer maxWidth='md'>
+      <Flex
+        justifyContent='space-between'
+        alignItems='flex-start'
+      >
+        <div>
+          <Avatar
+            src={profileImage}
+            size='sm'
+            alt='user image'
+            handleClick={() => {
+              alert(userId);
             }}
+          ></Avatar>
+        </div>
+        <Flex
+          direction='column'
+          ml={1}
+          style={{
+            width: '44rem',
+          }}
+        >
+          <FeedContentEditable
+            content={content}
+            onChange={(e: ChangeEvent<HTMLDivElement>) => {
+              handleInput(e);
+            }}
+          ></FeedContentEditable>
+          <Flex
+            mt={1.2}
+            style={{ height: '2.75rem' }}
           >
-            <FeedInput
-              value={text}
-              onChange={onChange}
-              placeholder='무엇을 하고 계신가요?'
-            ></FeedInput>
-            <Flex style={{ height: '2.75rem' }}>
-              <Button
-                variant='outlined'
-                size='md'
-                color='--primaryColor'
-                onClick={() => {
-                  alert('위치');
-                }}
-                mr={1.2}
-                style={{ height: '100%', width: '4.5rem' }}
-              >
-                위치
-              </Button>
-              <Button
-                variant='outlined'
-                size='md'
-                color='--primaryColor'
-                onClick={() => {
-                  alert('사진');
-                }}
-                style={{ height: '100%', width: '4.5rem' }}
-              >
-                사진
-              </Button>
-              <Button
-                type='submit'
-                variant='filled'
-                size='md'
-                color='--primaryColor'
-                style={{ height: '100%', width: '4.5rem', marginLeft: 'auto' }}
-              >
-                작성
-              </Button>
-            </Flex>
+            <Button
+              variant='outlined'
+              size='md'
+              color='--primaryColor'
+              onClick={() => {
+                alert('위치');
+              }}
+              mr={1.2}
+              style={{ height: '100%', width: '4.5rem' }}
+            >
+              위치
+            </Button>
+            <Button
+              variant='outlined'
+              size='md'
+              color='--primaryColor'
+              onClick={() => {
+                alert('사진');
+              }}
+              style={{ height: '100%', width: '4.5rem' }}
+            >
+              사진
+            </Button>
+            <Button
+              variant='filled'
+              size='md'
+              color='--primaryColor'
+              onClick={() => handleButtonClick()}
+              style={{ height: '100%', width: '4.5rem', marginLeft: 'auto' }}
+            >
+              작성
+            </Button>
           </Flex>
         </Flex>
-      </form>
-    </FeedItemContainer>
+      </Flex>
+    </FeedListInputContainer>
   );
 };
 
