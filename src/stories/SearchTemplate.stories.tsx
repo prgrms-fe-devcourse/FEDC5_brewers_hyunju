68bc5f7396ec80028ae76687bce071e39983c92e
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-01-11 11:12:18 +0900

    Docs: SearchTemplate 스토리 작성

--- src/stories/SearchTemplate.stories.tsx
+++ src/stories/SearchTemplate.stories.tsx
@@ -24,6 +24,20 @@ export default {
         };
       }
     }),
+    posts: Array.from(Array(10)).map((_, idx) => ({
+      id: `post_${idx}`,
+      userId: `user_id`,
+      profileImage: 'https://picsum.photos/200',
+      imageUrl: 'https://picsum.photos/500/300',
+      userName: `tester${idx}`,
+      createdAt: '2024-01-11',
+      updatedAt: '2024-01-11',
+      content: 'test post content',
+      likesCount: 53,
+      commentsCount: 33,
+      onFeedClick: () => {},
+      onUserClick: () => {},
+    })),
   },
 };
 
