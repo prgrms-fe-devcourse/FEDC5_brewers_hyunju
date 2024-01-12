9766baa13e6f3e9bde13b619186b7e59da59c107
yeonkyoung <keep.ykk@gmail.com>
2024-01-12 13:07:36 +0900

    Feat: SignupPage 추가. input 상태 form 및 input error message 상태 추가

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,10 +1,53 @@
-import Text from '~/components/common/Text';
+import { useState } from 'react';
 
 const SignupPage = () => {
+  const [userSignupInfo, setUserSignupInfo] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+    checkPassword: '',
+  });
+  const [formErrorMessage, setFormErrorMessage] = useState<string>('');
+  const [userSignupInfoIsError, setUserSignupInfoIsError] = useState({
+    fullName: false,
+    email: false,
+    password: false,
+    checkPassword: false,
+  });
+
+  const { handleSignup, status } = useSignup();
+
+  const onChange = (text: string, inputName: string) => {
+    setUserSignupInfo({ ...userSignupInfo, [inputName]: text });
+    setUserSignupInfoIsError({ ...userSignupInfoIsError, [inputName]: !text });
+    setFormErrorMessage('');
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (
+      userSignupInfo.fullName &&
+      userSignupInfo.email &&
+      userSignupInfo.password &&
+      userSignupInfo.checkPassword
+    ) {
+      handleSignup({
+        email: userSignupInfo.email,
+        password: userSignupInfo.password,
+        fullName: userSignupInfo.fullName,
+      });
+    }
+  };
+
   return (
-    <>
-      <Text>Signup</Text>
-    </>
+    <SignupTemplate
+      onChange={onChange}
+      onSubmit={onSubmit}
+      userSignupInfo={userSignupInfo}
+      status={status}
+      formErrorMessage={formErrorMessage}
+      userSignupInfoIsError={userSignupInfoIsError}
+    />
   );
 };
 
