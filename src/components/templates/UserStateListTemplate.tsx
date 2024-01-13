c4aec87e75c254cb4ed9f8c24b1158c2ec56a5f9
yeonkyoung <keep.ykk@gmail.com>
2024-01-13 13:42:35 +0900

    Feat: UserStateListTemplate 추가

---
+++ src/components/templates/UserStateListTemplate.tsx
@@ -0,0 +1,107 @@
+import styled from '@emotion/styled';
+
+import Container from '~/components/common/Container';
+import Flex from '~/components/common/Flex';
+import UserStateListItem from '~/components/userListItem/UserStateListItem';
+import Text from '~/components/common/Text';
+import CircleLoading from '~/components/loading/CircleLoading';
+
+import { UserType } from '~/types/common';
+
+export interface UserStateListTemplatePropsType {
+  onlineUserList: UserType[];
+  offlineUserList: UserType[];
+  status: 'stale' | 'loading' | 'error' | 'success';
+}
+
+const ContentDiv = styled.div`
+  overflow: auto;
+
+  width: 12rem;
+  height: 35.75rem;
+  padding: 1rem;
+  border: 1px solid var(--primaryColor);
+  box-shadow: 3px 3px 5px var(--primaryColor);
+
+  overflow-x: hidden;
+
+  &::-webkit-scrollbar {
+    width: 0.25rem;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background: var(--primaryColor);
+  }
+
+  &::-webkit-scrollbar-track {
+    background: var(--red100);
+  }
+`;
+
+const UserStateListTemplate = ({
+  onlineUserList,
+  offlineUserList,
+  status,
+}: UserStateListTemplatePropsType) => {
+  return (
+    <Container maxWidth='sm'>
+      <ContentDiv>
+        <Flex
+          direction='column'
+          gap={1}
+        >
+          <Flex
+            direction='column'
+            gap={0.5}
+          >
+            <Text
+              size='sm'
+              style={{ backgroundColor: 'var(--green100)' }}
+            >
+              온라인
+            </Text>
+            <Flex
+              direction='column'
+              gap={0.25}
+            >
+              {status === 'loading' ? (
+                <CircleLoading size={1} />
+              ) : (
+                onlineUserList?.map((user) => (
+                  <UserStateListItem
+                    key={user._id}
+                    fullName={user.fullName}
+                    src={user.image}
+                    isOnline={user.isOnline}
+                  />
+                ))
+              )}
+            </Flex>
+          </Flex>
+          <Flex
+            direction='column'
+            gap={0.5}
+          >
+            <Text
+              size='sm'
+              style={{ backgroundColor: 'var(--red100)' }}
+            >
+              오프라인
+            </Text>
+            {offlineUserList?.map((user) => (
+              <UserStateListItem
+                key={user._id}
+                fullName={user.fullName}
+                src={user.image}
+                isOnline={user.isOnline}
+              />
+            ))}
+          </Flex>
+        </Flex>
+      </ContentDiv>
+    </Container>
+  );
+};
+
+export default UserStateListTemplate;
