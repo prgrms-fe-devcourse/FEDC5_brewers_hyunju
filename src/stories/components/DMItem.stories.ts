ce50b7df93e8b17c86b54f64faeee6d02d564f83
yeonkyoung <keep.ykk@gmail.com>
2024-01-13 20:55:13 +0900

    Refactor: 변경된 props 반영

--- src/stories/components/DMItem.stories.ts
+++ src/stories/components/DMItem.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import DMItem from '~/components/directMessage/DMItem';
 
 const meta: Meta<typeof DMItem> = {
-  title: 'Components/Common/DMItem',
+  title: 'Component/DMItem',
   component: DMItem,
   tags: ['autodocs'],
   argTypes: {},
@@ -15,31 +15,9 @@ type Story = StoryObj<typeof DMItem>;
 export const Default: Story = {
   args: {
     userName: '사용자',
-    userNameSize: 'md',
-    userNameWeight: 600,
-    userNameColor: '--adaptive900',
-    message: `Lorem ipsum, dolor sit amet consectetur adipisicing elit`,
-    messageSize: 'md',
-    messageWeight: 400,
-    messageColor: '--adaptive900',
-    borderColor: '--primaryColor',
-    avatar: '',
-  },
-};
-
-export const Standard: Story = {
-  args: {
-    userName: '사용자',
-    userNameSize: 'md',
-    userNameWeight: 600,
-    userNameColor: '--adaptive900',
-    message: `Lorem ipsum, dolor sit amet consectetur adipisicing elit`,
-    messageSize: 'md',
-    messageWeight: 400,
-    messageColor: '--adaptive900',
-    borderColor: '--primaryColor',
-    alertColor: '--red600',
-    alertCount: 100,
-    avatar: 'avatar',
+    message: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.`,
+    size: 'lg',
+    src: 'https://picsum.photos/200',
+    seen: false,
   },
 };
