75323644f09a0ce258054b5ab741cccae27d313a
yeonkyoung <keep.ykk@gmail.com>
2024-01-15 01:02:11 +0900

    [58] DM 목록 템플릿 구현 (#125)
    * Feat: DMItem 컴포넌트 구현
    
    * Refactor: props 변경, Badge 위치 및 내용 변경
    
    * Refactor: 변경된 props 반영
    
    * Feat: DM 목록 템플릿 구현

---
+++ src/components/templates/DMListTemplate.tsx
@@ -0,0 +1,53 @@
+import Container from '~/components/common/Container';
+import CircleLoading from '~/components/loading/CircleLoading';
+import DMItem from '~/components/directMessage/DMItem';
+import Flex from '~/components/common/Flex';
+import Text from '~/components/common/Text';
+
+import { ConversationType } from '~/types/common';
+
+export interface DMListTemplatePropsType {
+  conversations: ConversationType[];
+  status: 'stale' | 'loading' | 'error' | 'success';
+}
+const DMListTemplate = ({ conversations, status }: DMListTemplatePropsType) => {
+  return (
+    <Container
+      maxWidth='sm'
+      minWidth={20}
+      minHeight={40}
+    >
+      <Flex
+        direction='column'
+        gap={1}
+      >
+        <Text size='2xl'>채팅 목록</Text>
+        <Flex
+          direction='column'
+          gap={0.25}
+        >
+          {status === 'loading' && <CircleLoading size={1} />}
+          {status === 'success' && conversations.length ? (
+            conversations?.map(({ message, sender, seen }, index) => (
+              <DMItem
+                key={index}
+                userName={sender.fullName}
+                message={message}
+                seen={seen}
+                src={sender.image}
+              />
+            ))
+          ) : (
+            <Flex
+              justifyContent='center'
+              mt={10}
+            >
+              <Text color='--adaptive400'>메시지함이 비었습니다</Text>
+            </Flex>
+          )}
+        </Flex>
+      </Flex>
+    </Container>
+  );
+};
+export default DMListTemplate;
