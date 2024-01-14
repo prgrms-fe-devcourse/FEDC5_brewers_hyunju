02ca66b3aa4d8b3f15f18565e480bd07c760fab2
yeonkyoung <keep.ykk@gmail.com>
2024-01-15 01:30:54 +0900

    Feat: 개인 대화 목록 컴포넌트 구현

---
+++ src/components/PersonalConversation.tsx
@@ -0,0 +1,42 @@
+import PersonalMessage from './PersonalMessage';
+import Flex from './common/Flex';
+
+import { MessageType } from '~/types/common';
+
+interface PersonalConversationPropsType {
+  messages: MessageType[];
+  userId: string | undefined;
+}
+
+const PersonalConversation = ({
+  messages,
+  userId,
+}: PersonalConversationPropsType) => {
+  const convertTime = (createdAt: string) => {
+    return createdAt;
+  };
+
+  return (
+    <Flex
+      direction='column'
+      gap={0.25}
+    >
+      {messages?.map((message, idx) => (
+        <PersonalMessage
+          message={message.message}
+          subject={message.sender._id === userId ? 'you' : 'me'}
+          createdAt={
+            idx < messages.length - 1
+              ? convertTime(messages[idx + 1].createdAt) ===
+                  convertTime(message.createdAt) &&
+                messages[idx + 1].sender._id === message.sender._id
+                ? null
+                : convertTime(message.createdAt)
+              : convertTime(message.createdAt)
+          }
+        />
+      ))}
+    </Flex>
+  );
+};
+export default PersonalConversation;
