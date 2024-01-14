a7307d5f27dea2b4ec14b137008d5f3d38bc5112
yeonkyoung <keep.ykk@gmail.com>
2024-01-15 01:52:33 +0900

    Feat: 메시지 전송 페이지 구현

---
+++ src/pages/PresonalMessagePage.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import MessageSendingTemplate from '~/components/templates/MessageSendingTemplate';
+
+import usePersonalMessageList from '~/hooks/api/conversation/usePersonalMessageList';
+
+const PersonalMessagePage = () => {
+  const { handlePersonalMessageList, status, data } = usePersonalMessageList();
+  const { userId } = useParams();
+
+  useEffect(() => {
+    if (!userId) {
+      return;
+    }
+    handlePersonalMessageList({
+      userId,
+    });
+  }, [userId]);
+
+  useEffect(() => {
+    if (!userId) {
+      return;
+    }
+    const timer = setInterval(() => {
+      handlePersonalMessageList({
+        userId,
+      });
+    }, 60000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <MessageSendingTemplate
+      messageListStatus={status}
+      messageListData={data}
+      userId={userId}
+      handlePersonalMessageList={handlePersonalMessageList}
+    />
+  );
+};
+
+export default PersonalMessagePage;
