80bd9525139d11bad1799c35e52dbe751a0833c5
yeonkyoung <keep.ykk@gmail.com>
2024-01-15 01:01:31 +0900

    [32] DMItem 컴포넌트 구현 (#55)
    * Feat: DMItem 컴포넌트 구현
    
    * Refactor: props 변경, Badge 위치 및 내용 변경
    
    * Refactor: 변경된 props 반영

---
+++ src/stories/components/DMItem.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import DMItem from '~/components/directMessage/DMItem';
+
+const meta: Meta<typeof DMItem> = {
+  title: 'Component/DMItem',
+  component: DMItem,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof DMItem>;
+
+export const Default: Story = {
+  args: {
+    userName: '사용자',
+    message: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.`,
+    size: 'lg',
+    src: 'https://picsum.photos/200',
+    seen: false,
+  },
+};
