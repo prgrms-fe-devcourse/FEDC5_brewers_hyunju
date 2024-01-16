28bab7fdb158728eacc8eadc90838576be357ebf
bel1c10ud <mail@bellclou.de>
2024-01-16 23:53:28 +0900

    Feat: 알림 전체 읽음 처리 기능 추가

--- src/components/notification/NotificationItem.tsx
+++ src/components/notification/NotificationItem.tsx
@@ -3,12 +3,27 @@ import Avatar from '../common/Avatar';
 import Flex from '../common/Flex';
 import Text from '../common/Text';
 import Skeleton from '../common/Skeleton';
+import { useMemo } from 'react';
 
 export interface NotificationItemPropsType {
   data: NotificationType;
 }
 
 const NotificationItem = ({ data }: NotificationItemPropsType) => {
+  const type = useMemo(() => {
+    if (data.message) return 'MESSAGE';
+    else if (data.comment) return 'COMMENT';
+    else if (data.follow) return 'FOLLOW';
+    else return 'LIKE';
+  }, [data]);
+
+  const message = {
+    MESSAGE: '메시지를 보냈습니다.',
+    COMMENT: '댓글을 남겼습니다.',
+    FOLLOW: '팔로우 했습니다.',
+    LIKE: '좋아요를 누르셨습니다.',
+  };
+
   return (
     <Flex
       alignItems='center'
@@ -24,8 +39,10 @@ const NotificationItem = ({ data }: NotificationItemPropsType) => {
         direction='column'
         gap={0.25}
       >
-        <Text size='md'>{data.author.fullName}</Text>
-        <Text size='sm'>{data.author.fullName}님께서 ...</Text>
+        <Text size='md'>{type}</Text>
+        <Text size='sm'>
+          {data.author.fullName}님께서 {message[type]}
+        </Text>
       </Flex>
     </Flex>
   );
