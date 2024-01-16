6ede109a79a701def6302c9b1c63976008e4c52e
bel1c10ud <mail@bellclou.de>
2024-01-16 13:35:00 +0900

    Refactor: 로그인 하지 않은 경우에 대한 예외처리

--- src/pages/NotificationPage.tsx
+++ src/pages/NotificationPage.tsx
@@ -1,15 +1,20 @@
 import { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
 import NotificationSkeleton from '~/components/templates/NotificationSkeleton';
 import NotificationTemplate from '~/components/templates/NotificationTemplate';
+import RequiredLoginTemplate from '~/components/templates/RequiredLoginTemplate';
 import useGetNotification from '~/hooks/api/notification/useGetNotification';
+import { userState } from '~/recoil/login/atoms';
 
 const NotificationPage = () => {
+  const auth = useRecoilValue(userState);
   const { data, getNotification } = useGetNotification();
 
   useEffect(() => {
     getNotification();
   }, []);
 
+  if (!auth) return <RequiredLoginTemplate />;
   if (!data) return <NotificationSkeleton />;
   return <NotificationTemplate data={data} />;
 };
