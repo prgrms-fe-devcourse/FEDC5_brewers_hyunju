d92ea540a3b609919adc9eee54e6f180b8b8b40d
bel1c10ud <86952779+bel1c10ud@users.noreply.github.com>
2024-01-17 13:14:48 +0900

    Feat: 404, 401 탬플릿 추가 (#222)

---
+++ src/components/templates/NotFoundTemplate.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
+import Button from '~/components/common/Button';
+import StyledContainer from '~/components/common/Container';
+import NavBar from '~/components/common/NavBar';
+import Text from '~/components/common/Text';
+
+const NotFoundTemplate = () => {
+  const navigator = useNavigate();
+
+  return (
+    <>
+      <NavBar />
+      <Container
+        maxWidth='md'
+        p={2}
+      >
+        <Text
+          size='2xl'
+          weight={800}
+        >
+          404 Not Found
+        </Text>
+        <Text>페이지를 찾을 수 없어요.</Text>
+        <Button
+          variant='filled'
+          size='md'
+          color='--primaryColor'
+          onClick={() => navigator('/')}
+        >
+          홈으로 돌아가기
+        </Button>
+      </Container>
+    </>
+  );
+};
+
+export default NotFoundTemplate;
+
+const Container = styled(StyledContainer)`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  border-radius: 1rem;
+
+  gap: 1rem;
+`;
--- src/pages/PostEditPage.tsx
+++ src/pages/PostEditPage.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
-import useGetPostDetail from '~/hooks/api/post/useGetPostDetail';
-import { userState } from '~/recoil/login/atoms';
-import ErrorPage from './ErrorPage';
 import PostEditTemplate from '~/components/templates/PostEditTemplate';
 import RequiredLoginTemplate from '~/components/templates/RequiredLoginTemplate';
+import UnauthorizedTemplate from '~/components/templates/UnauthorizedTemplate';
+import useGetPostDetail from '~/hooks/api/post/useGetPostDetail';
+import { userState } from '~/recoil/login/atoms';
 
 const PostEditPage = () => {
   const { postId } = useParams();
@@ -23,7 +23,7 @@ const PostEditPage = () => {
   if (!auth) return <RequiredLoginTemplate />;
   else if (data) {
     if (data.author._id === auth._id) return <PostEditTemplate data={data} />;
-    else return <ErrorPage />;
+    else return <UnauthorizedTemplate />;
   } else return <></>;
 };
 
